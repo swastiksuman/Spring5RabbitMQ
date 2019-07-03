619fd7e9b1f6353676034bab3ee71c83e88c7ec8
Swastik Panda <swastiksuman@gmail.com>
2019-07-04 01:02:40 +0530

    sample ajax call

--- src/main/java/com/panda/socketprogramming/controller/IndexController.java
+++ src/main/java/com/panda/socketprogramming/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,5 +15,10 @@ public class IndexController {
         model.addAttribute("message", "Hello!");
         return "index";
     }
-
+    
+    @RequestMapping("/test")
+    @ResponseBody
+    public String getTest() {
+    	return "My Test Data";
+    }
 }
